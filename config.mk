141ccceac1d67c7c4af9db172280107f19f442dd
Niels Robin-Aubertin <niels.robin.aubertin@gmail.com>
2018-11-22 20:51:55 +0100

    Rework Makefile

--- config.mk
+++ config.mk
@@ -1,12 +1,6 @@
 # st version
 VERSION = 0.8.1
 
-# Customize below to fit your system
-
-# paths
-PREFIX = /usr/local
-MANPREFIX = $(PREFIX)/share/man
-
 X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
 
